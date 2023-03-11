5d0f0d16e4da9001141e66cf79f8280a003a684e
Jonald Damaso <damasojonald@gmail.com>
2023-03-11 00:19:03 +0000

    Build: done exercise1 asyn & await try catch

--- exercise001.js
+++ exercise001.js
@@ -11,11 +11,39 @@ const promise = new Promise((resolve, reject) => {
 	}
 });
 
+const getResponse = () => {
+	return  new Promise((resolve, reject) => {
+		// Generate random number between 0 and 9
+		const randomInt = Math.floor(Math.random() * 10);
+	
+		if (randomInt % 2 === 0) {
+			// Success
+			setTimeout(() => resolve('done'), 2000);
+		} else {
+			// Failure
+			setTimeout(() => reject('error'), 2000);
+		}
+	});
+};
 // Your solution(s) to exercise001 here!
-promise.then((response) => {
-	console.log(`Yay! Promise resolved with response: ${response}`);
-})
-.catch((response) => {
-	console.log(`Boo. Promise rejected with response: ${response}`);
-})
-console.log('Some function after the promise is executed');
+// promise.then((response) => {
+// 	console.log(`Yay! Promise resolved with response: ${response}`);
+// })
+// .catch((response) => {
+// 	console.log(`Boo. Promise rejected with response: ${response}`);
+// })
+// console.log('Some function after the promise is executed');
+
+// async and await
+
+
+const getPromiseResponse = async () => {
+	try{
+	const result = await getResponse();
+	console.log(`Yay! Promise resolved with response: ${result}`);
+	}catch(result){
+		console.log(`Boo. Promise rejected with response: ${result} `);
+	}
+	
+};
+getPromiseResponse();
